31b759f27d48a132c71c20ee0c15534828c14482
ZhexembinDias <zhexembin.jr@gmail.com>
2023-05-23 14:59:27 +0600

    hw_22.05

---
+++ homework/hw_22.05/task1.py
@@ -0,0 +1,9 @@
+a = float(input("Введите а "))
+b = float(input("Введите b "))
+c = float(input("Введите c "))
+
+if a + b > c and a + c > b and c + b > a:
+    print("True")
+else:
+    print("False")
+
---
+++ homework/hw_22.05/task4.py
@@ -0,0 +1,7 @@
+a = int(input("Введите число a "))
+b = int(input("Введите число b "))
+
+if a > b:
+    print("True")
+else:
+    print("False")
